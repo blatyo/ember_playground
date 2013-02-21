51735f013061388295c623a84047eb3d4c078ea8
Allen Madsen <blatyo@gmail.com>
2013-02-21 08:19:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberPlayground::Application.config.secret_token = 'a6bbf1ba7de30b9422243a9c6c7330e75e71ecddb36c768ad6af19b9a38b207fc5577f7030e9b4422feebddc39b43d76e2b7896c46d235b13156edc1175eca73'
